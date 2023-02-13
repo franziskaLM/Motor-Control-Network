a979c6f5eebd7e019b38ee2a0d7b6304ac8c2917
franziskaLM <117356527+franziskaLM@users.noreply.github.com>
2023-02-13 10:14:09 +0100

    Update rate-model.py

--- rate-model.py
+++ rate-model.py
@@ -1,141 +1,245 @@
-import numpy as np
 import random
-import least_squares_regression
-import initial_states
-import rate_model
-import matrix_preparation
+
 import matplotlib.pyplot as plt
-
-# System Parameter of the Rate Model
-N = 100  # number of neurons
-n_ex = N / 2  # number of exhibitory neurons
-n_in = N - n_ex  # number of inhibitory neurons
-r_0 = 5  # base rate in Hz
-r_max = 100  # max. rate in Hz
-tau = 200  # time - const of neuron membran in ms
-simulation_time = 1500  # in ms
-delta_t = 1  # duration of a time-step
-
-# Stimulus Parameter
-state_number = 0  # set the initial state, e.g. a0
-t_go = 1000  # point of time of the go cue # in ms
-tau_before_go = 400  # time const of the rise during preparation time
-tau_after_go = 2  # time const of the decay after go cue
-
-# trajectory params
-end_coord = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
-numbers_of_trajectories = len(end_coord)
-
-# train system
-number_of_repetitions = 50
-
-# initial systems
-matrix = matrix_preparation.main(N, n_ex, n_in)  # optimized connectivity matrix
-
-params = {"N": N, "n_ex": n_ex, "n_in": n_in, "r_0": r_0, "r_max": r_max, "tau": tau,
-          "simulation_time": simulation_time, "delta_t": delta_t,
-          "t_go": t_go, "tau_before_go": tau_before_go,
-          "tau_after_go": tau_after_go,
-          "number_of_repetitions": number_of_repetitions,
-          "number_of_trajectories": numbers_of_trajectories}
+import numpy as np
+import math
+import scipy.stats as ss
 
 
-def create_initial_conditions(a1, a2):
+def ornstein_uhlenbeck():
     """
-    Args:
-        a1: initial state a1
-        a2: initial state a2
+    Independent Ornstein-Uhlenbeck process for simulating the Noise-Input for each neuron
     Returns:
-        array with random initial states formed by linear combination from a1 and a2
-    """
-    states = []
-    # create for each trajectory one random state condition b
-    for k in range(numbers_of_trajectories):
-        states.append(
-           random.choice([1, -1]) * random.uniform(0.5, 1) * np.array(a1) + random.choice([1, -1]) * random.uniform(
-                0.5, 1) * np.array(a2))
-    return (np.array(states))
-
-
-def linear_trajectory():
-    """
-    creates linear trajectory for each given end-coordinate with start in (0,0)
-    Returns: list with tuples of (x_coordinates, y_coordinates)
-    """
-    trajectories_x = []  # array with x-coord of all trajectories
-    trajectories_y = []  # array with y-coord of all trajectories
-    for count, direction in enumerate(end_coord):
-        x_coord = np.zeros((int((simulation_time - t_go) / delta_t), 1))
-        y_coord = np.zeros((int((simulation_time - t_go) / delta_t), 1))
-        end_x = direction[0]
-        end_y = direction[1]
-        step_length_x = end_x / int((simulation_time - t_go) / delta_t)
-        step_length_y = end_y / int((simulation_time - t_go) / delta_t)
-        for step in range(1, (int((simulation_time - t_go) / delta_t))):
-            x_coord[step] = x_coord[step - 1] + step_length_x
-            y_coord[step] = y_coord[step - 1] + step_length_y
-        trajectories_x.append(x_coord)
-        trajectories_y.append(y_coord)
-    return (trajectories_x, trajectories_y)
-
-
-def create_b(b1, b2):
-    """
-    Args:
-        b1: bias for x-dim
-        b2: bias for y-dim
-    Returns:
-         array in size(2,T) where T: number of time-steps
-    """
-    b1 = np.ones((1, int(simulation_time - t_go / delta_t))) * b1[0]
-    b2 = np.ones((1, int(simulation_time - t_go / delta_t))) * b2[0]
-    return np.vstack((b1, b2))
-
-
-def create_m(m1, m2):
-    return np.vstack((m1.T, m2.T))
-
-
-def paint_trajectory(m1, b1, m2, b2, condition):
+        Array with arrays in number of simulated time steps dt.
+        Each array contains the noise value in number of Neurons for one time.
     """
 
-    :param m1: opt. readout weights x coord
-    :param b1: const. bias x-coord
-    :param m2: opt. readout weights y coord
-    :param b2: const bias y coord
-    :param condition: initial condition
-    :return: trajectory for initial condition
-    """
-    params["number_of_repetitions"] = 1
-    delta_r = rate_model.safe_data(matrix, [condition], params).T[:, int(t_go / delta_t):]
-    # print(np.shape(delta_r))
-    b = create_b(b1, b2)
-    m = create_m(m1, m2)
-    z1,z2 = m @ delta_r + b
-    return (z1,z2)
+    time_steps = int(simulation_time / delta_t) + 1  # number of time steps
+    T_vec, dt = np.linspace(0, simulation_time, time_steps, retstep=True)
 
-def paint_all_in_one(m1,b1,m2,b2,conditions):
-    for condition in conditions:
-        z1,z2 = paint_trajectory(m1,b1,m2,b2,condition)
-        plt.plot(z1,z2)
+    kappa = 20  # mean reversion coefficient in Hz
+    theta = 0  # long term mean in Hz
+    sigma = 1.5
+    # std_asy = np.sqrt(sigma ** 2 / (2 * kappa))  # asymptotic standard deviation 0.2Hz
+
+    X0 = 0  # start value in Hz
+    X = np.zeros((N, time_steps))
+    X[:, 0] = X0
+    W = ss.norm.rvs(loc=0, scale=1, size=(N, time_steps - 1))
+    std_dt = np.sqrt(sigma ** 2 / (2 * kappa) * (1 - np.exp(-2 * kappa * dt)))
+
+    for t in range(0, time_steps - 1):
+        X[:, t + 1] = theta + np.exp(-kappa * dt) * (X[:, t] - theta) + std_dt * W[:, t]
+
+    return X
+
+
+######################################################################################
+
+
+class Network:
+
+    def __init__(self, initial_state, dot_product):
+
+        self.state_space = -0.5 + np.random.rand(N, 1)
+        self.noise = ornstein_uhlenbeck()
+        self.delta_r = np.zeros((N, 1))
+        self.update_delta_r()
+        self.initial_state = initial_state
+        self.matrix_dot_g = dot_product
+        self.stimulus = self.initial_state - self.matrix_dot_g
+        self.X = np.zeros((int(simulation_time / delta_t), N))
+        self.time = 0
+
+    def __str__(self):
+        return f"Steady-State: {self.state_space}"
+
+    def get_steady_state(self):
+        """
+        Returns:
+            actual state space of the network
+        """
+        return self.state_space
+
+    def get_time(self):
+        """
+        Returns:
+            actual time-step of the network
+        """
+        return self.time
+
+    def get_noise(self):
+        """
+        Returns:
+            N-dim array with random independent noise-term for each neuron
+        """
+        simulation_step = int(self.time / delta_t)
+        # print("Noise:"+str(self.noise[simulation_step]))
+        return self.noise[:, simulation_step]
+
+    def update_delta_r(self):
+        """
+        Returns:
+            vector of the instantaneous singel-unit firing rate
+        """
+        for i, unit in enumerate(self.state_space):
+            if unit[0] < 0:
+                self.delta_r[i] = r_0 * math.tanh(unit[0] / r_0)
+            else:
+                self.delta_r[i] = (r_max - r_0) * math.tanh(unit[0] / (r_max - r_0))
+
+    def get_input(self):
+        """
+        Returns:
+            dot product of connectivity matrix * firing rate + constant input
+        """
+        return (matrix @ self.delta_r) + 10* self.get_noise() + self.stimulus
+
+    def get_delta_r(self):
+        return self.delta_r
+
+    def get_saved_data(self):
+        return self.X
+
+    def update_stimulus(self):
+        if self.time < t_go:
+            self.stimulus = math.exp(self.time / tau_before_go) * (self.initial_state - self.matrix_dot_g)
+        else:
+            self.stimulus = math.exp(- self.time / tau_after_go) * (self.initial_state - self.matrix_dot_g)
+
+    def update_steady_state(self):
+        """
+        Returns:
+            updated steady-state
+        """
+        self.state_space = self.state_space + delta_t * (-self.state_space / tau) + delta_t / tau * self.get_input()
+
+    def update_time(self):
+        """
+        Returns:
+            updated time step
+        """
+        self.time += delta_t
+
+    def safe_data(self):
+        for i, unit in enumerate(self.delta_r):
+            self.X[self.time][i] = unit
+
+    def update(self):
+        self.safe_data()
+        self.update_steady_state()
+        self.update_delta_r()
+        self.update_stimulus()
+        # print(self.stimulus+self.state_space)
+        self.update_time()
+
+    def run(self):
+        while self.time < simulation_time:
+            self.update()
+
+    def show_run(self):
+        x_data = []
+        y_data = []
+        for unit in range(N):
+            x_data.append([])
+        while self.time < simulation_time:
+            for i in range(N):
+                x_data[i].append(self.delta_r[i][0])
+            y_data.append(self.time)
+            self.update()
+        for i in range(0, len(x_data)):
+            plt.plot(y_data, x_data[i], label=str(unit))
+        plt.show()
+
+
+####################################################################################
+def calculate_g(condition):
+    """
+    Returns: gain function for calculation of the projection weights P(a) = a - W*g(a)
+    depending on the initial state a
+    """
+    g = np.zeros((N, 1))
+    for i, unit in enumerate(condition):
+        if unit[0] < 0:
+            g[i] = r_0 * math.tanh(unit[0] / r_0)
+        else:
+            g[i] = (r_max - r_0) * math.tanh(unit[0] / (r_max - r_0))
+    return g
+
+
+def matrix_dot_g(matrix, condition):
+    """
+    Returns: dot product of the connectivity matrix and the gain function g
+    """
+    g = calculate_g(condition)
+    # print(condition)
+    # print(g)
+    return matrix @ g
+
+def show(data):
+    t_data = []
+    r_data = []
+    t = 0
+    while t < int(simulation_time / delta_t):
+        t_data.append(t)
+        t += delta_t
+    for neuron in range(N):
+        r_data.append(data[:, neuron])
+    for data in r_data:
+        plt.plot(t_data, data)
     plt.show()
 
 
+def show_data(X):
+    i = 0
+    while i < np.shape(X)[0]:
+        actual_run = X[i:i + int(simulation_time / delta_t), :]
+        show(actual_run)
+        i += int(simulation_time / delta_t)
 
-def train(matrix, conditions, params):
-    return rate_model.safe_data(matrix, conditions, params)
+def safe_data(connectivity_weights, conditions, parameter):
+    """
 
-def main():
+    Returns:
+        array in size(time* conditions * repetitions, N)
+        with saved fire-rates delta_r for each condition and repetition over simulation time t
+
+    """
     global matrix
-    matrix = matrix_preparation.main(N, n_ex, n_in)  # optimized connectivity matrix
-    trajectories = linear_trajectory()
-    initial_conditions = create_initial_conditions(initial_states.main(N, matrix)[0], initial_states.main(N, matrix)[1])
+    global number_of_repetitions, N, n_ex, n_in, r_0, r_max, tau, simulation_time, delta_t, t_go, tau_before_go, tau_after_go
 
-    X = train(matrix, initial_conditions,params)
-    m1, b1, m2, b2 = least_squares_regression.main(X, trajectories, params)
+    matrix = connectivity_weights
+    initial_states = conditions
+    N = parameter["N"]
+    n_ex = parameter["n_ex"]  # number of exhibitory neurons
+    n_in = parameter["n_in"]  # number of inhibitory neurons
+    r_0 = parameter["r_0"]  # base rate in Hz
+    r_max = parameter["r_max"]  # max. rate in Hz
+    tau = parameter["tau"]  # time - const of neuron membran in ms
+    simulation_time = parameter["simulation_time"]  # in ms
+    delta_t = parameter["delta_t"]  # duration of a time-step
 
-    paint_all_in_one(m1,b1,m2,b2,initial_conditions)
+    # Stimulus Parameter
+    t_go = parameter["t_go"]  # point of time of the go cue # in ms
+    tau_before_go = parameter["tau_before_go"]  # time const of the rise during preparation time
+    tau_after_go = parameter["tau_after_go"]  # time const of the decay after go cue
+    number_of_repetitions = parameter["number_of_repetitions"]  # number of repetitions
+
+
+    all_X = []
+
+    for condition in initial_states:
+        for repeat in range(number_of_repetitions):
+            network = Network(condition, matrix_dot_g(matrix,condition))
+            network.run()
+            all_X.append(network.get_saved_data())
+    X = all_X[0]
+    for elem in all_X[1:]:
+        X = np.vstack((X, elem))
+    #show_data(X)
+    return X
 
 
 
-main()
+
+
